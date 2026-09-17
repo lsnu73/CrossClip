05bef846ee8831135f177715fb34f942d2ce5a07
王海瑞 <2022174946@qq.com>
2026-09-17 09:56:29 +0800

    chore(android): 版本升级 2.3.0 (versionCode 24)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.crossclip.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "2.2.2"
+        versionCode = 24
+        versionName = "2.3.0"
     }
 
     signingConfigs {
